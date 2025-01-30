c6d9265e0dca07362498bd3d46f859fe92f373d4
MarioYellowy <mario03eche@gmail.com>
2025-01-29 23:55:08 -0600

    Update: Diagrama classes added(not complete)

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
 
+        Menu newMenu = new Menu();
+
     }
 }
---
+++ src/Menu.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Menu {
+    Menu() {
+        System.out.println("Welcome, select a option:");
+        Scanner scanner = new Scanner(System.in);
+        Integer input = scanner.nextInt();
+
+    }
+
+
+}
